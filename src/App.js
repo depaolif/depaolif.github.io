db6b8ff7340d483b49e0cd1dccfd33479748ffbb
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-04-26 10:50:39 -0400

    implemented routers

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import NavBar from './components/navbar'
 import About from './components/about'
 import { Grid, Row, Col } from 'react-bootstrap';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import './App.css'
 
 
 class App extends Component {
   render() {
     return (
-      <Grid className="app">
-        <Row >
-          <Col md={4}><NavBar /></Col>
-          <Col md={8}><About /></Col>
-        </Row>
-      </Grid>
+      <HashRouter>
+        <Grid>
+          <Row>
+            <Col md={4} >
+              <NavBar />
+            </Col>
+            <Col md={8} >
+              <About />
+            </Col>
+          </Row>
+        </Grid>
+      </HashRouter>
     );
   }
 }
