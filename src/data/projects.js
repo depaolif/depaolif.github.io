377269e9f958affea137d0f245d24bc509b23d6d
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-05-01 20:07:37 -0400

    elaborated on APIs used in Rails project

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,14 +9,14 @@ const ProjectData = [
   {
     "title": "Koko",
     "description": "Dynamic, social website where users review songs and vote on others’ reviews. Calls the Spotify API to allow users to play songs as they write their reviews. Reviews are shared on song pages and on user's profiles. Public pages show top 'influencers' and trending reviews/songs.",
-    "languages": ["Ruby", "Ruby on Rails", "SQL", "HTML5", "SCSS", "JavaScript", "jQuery", "React", "Redux"],
+    "languages": ["Ruby", "Ruby on Rails", "SQL", "API's", "HTML5", "SCSS", "JavaScript", "jQuery", "React", "Redux"],
     "external apis": ["Spotify"],
     "github link": "https://github.com/depaolif/yelpify"
   },
   {
     "title": "CheckUpOn",
     "description": "Single-page webapp that displays a Twitter user’s current mood. Calls the Twitter API using a twitter handle to get that user's last 10 tweets, then runs them through Google's Natural Language API. Ultimately returns a short blurb and stats on the user's most recent general 'mood'.",
-    "languages": ["HTML5", "SCSS", "JavaScript", "jQuery"],
+    "languages": ["Ruby", "Ruby on Rails", "API's", "HTML5", "SCSS", "JavaScript", "jQuery"],
     "external apis": ["Twitter", "Google Natural Language"],
     "github link": "https://github.com/depaolif/check-up-on"
   }
