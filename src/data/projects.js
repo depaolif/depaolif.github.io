d91814035dace96f30c0e4b61eb1c46fc4c88b52
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-05-03 19:46:26 -0400

    added descriptions for Koko and CheckUpOn;

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,6 +10,7 @@ const ProjectData = [
   {
     "title": "Koko",
     "summary": "Dynamic, social website where users review songs and vote on others’ reviews. Calls the Spotify API to allow users to play songs as they write their reviews. Reviews are shared on song pages and on user's profiles. Public pages show top 'influencers' and trending reviews/songs.",
+    "description": "Koko is a music discovery application that utilizes Spotify’s API to allow users to search for songs, add reviews and up/down vote other users’ reviews. The application leverages a users’ voting data to provide them with a dashboard of curated suggestions, as well as views for the songs and artists trending across the site. Every review is given a reliability rating based on other users' upvoting or downvoting of that review, leading to weighted reviews. Highly rated reviews are thereby given more weight in determining top songs and artists for a given period.",
     "languages": ["Ruby", "Ruby on Rails", "SQL", "API's", "HTML5", "SCSS", "JavaScript", "jQuery", "React", "Redux"],
     "externalApis": ["Spotify"],
     "githubLink": "https://github.com/depaolif/yelpify"
@@ -17,6 +18,7 @@ const ProjectData = [
   {
     "title": "CheckUpOn",
     "summary": "Single-page webapp that displays a Twitter user’s current mood. Calls the Twitter API using a twitter handle to get that user's last 10 tweets, then runs them through Google's Natural Language API. Ultimately returns a short blurb and stats on the user's most recent general 'mood'.",
+    "description": "CheckUpOn is a single-page webapp that lets you see how a twitter user is doing. After inputting a user's twitter handle, an AJAX request is made to the Twitter API, retrieving that user's last 10 tweets. Those tweets are then condensed and sent to the Google Natural Languages API, which parses those tweets for emotional sentiment. Finally, a message is displayed on-screen, showing how that user is doing. What's shown is that user's emotional sentiment, representing how positive or negative their tweets were, and that user's emotional magnitude, representing how intense that user's emotion were.",
     "languages": ["Ruby", "Ruby on Rails", "API's", "HTML5", "SCSS", "JavaScript", "jQuery"],
     "externalApis": ["Twitter", "Google Natural Language"],
     "githubLink": "https://github.com/depaolif/check-up-on"
